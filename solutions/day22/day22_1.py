f79f7da816cc0a5f8bf56002202cb3b9c9b6e28f
MikeMNelhams <mikemnelhams@gmail.com>
2024-01-09 00:07:58 +0000

    Changelist:
    - Possible working solution.

--- solutions/day22/day22_1.py
+++ solutions/day22/day22_1.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 from handy_dandy_library.file_processing import read_lines
 from handy_dandy_library.string_manipulations import make_blue
 
+from collections import defaultdict
+from abc import abstractmethod
 from typing import Iterable
 
 
@@ -47,84 +49,158 @@ class Vector3D:
 
 
 class Brick:
-    def __init__(self, start: Vector3D, end: Vector3D):
+    def __init__(self, start: Vector3D, end: Vector3D, shape: Vector3D):
         self.start = start
         self.end = end
-        shape = end - start
-        self.shape_dimension = shape.first_non_zero_dim_index
         self.shape_length = shape.manhattan_distance_from_zero + 1
-        self.__unit_vector = Vector3D(tuple(1 if i == self.shape_dimension else 0 for i in range(3)))
-        shape.z = 0
-        self.xy_projection = shape
+        self.xy_projection = shape.xy_projection
 
     def __repr__(self) -> str:
         return f"{make_blue('B')}[{self.start} -> {self.end} @ {self.start.z}]"
-    
-    @staticmethod
-    def __on_segment(p, q, r) -> bool:
-        if max(p.x, r.x) >= q.x >= min(p.x, r.x) and max(p.y, r.y) >= q.y >= min(p.y, r.y):
-            return True
-        return False
 
-    def intersections(self, other: Brick) -> list[int]:
-        # # Returns the xy projection intersection
-        # if self.shape_dimension == 2:
-        #     # Both dots
-        #     if other.shape_dimension == 2:
-        #         if self.xy_projection == other.xy_projection:
-        #             return [0]
-        #     # ONLY Self dot
-        #     else:
-        #         valid_x = other.start.x <= self.xy_projection.x <= other.end.x
-        #         valid_y = other.start.y <= self.xy_projection.y <= other.end.y
-        #         if valid_x and valid_y:
-        #             return [0]
-        # # Other dot
-        # elif other.shape_dimension == 2:
-        #     valid_x = self.start.x <= other.xy_projection.x <= self.end.x
-        #     valid_y = self.start.y <= other.xy_projection.y <= self.end.y
-        #     if valid_x and valid_y:
-        #         return [0]
-        # # Parallel and neither are dots
-        # elif self.shape_dimension == other.shape_dimension:
-        #     if self.start.x <= other.start.x <= self.end.x:
-        #         return list(range(self.shape_length - (self.end.x - other.start.x),self.shape_length))
-        #     if self.start.y <= other.start.y <= self.end.x:
-        #         return list(range(self.shape_length - (self.end.y - other.start.y),self.shape_length))
-        #     if other.start.x <= self.start.x <= other.end.x:
-        #         return list(range(other.end.x - self.start.x))
-        #     if other.start.y <= self.start.y <= other.end.y:
-        #         return list(range(other.end.y - self.start.y))
-        # # Regular intersection between a horizontal and a vertical
-        # else:
-        #     self_is_horizontal = self.shape_dimension == 0
-        #     if self_is_horizontal:
-        #         if self.start.x <= other.xy_projection.x <= self.end.x:
-        #             return
-        #     else:
-        # return []
+    def is_below(self, point: Vector3D) -> bool:
+        return point.z > self.end.z
 
-#     |
-#     |
-# ----#----
-#     |
+    def frontier(self) -> list[Vector3D]:
+        points = [self.start + self.unit_vector * i for i in range(self.shape_length)]
+        points = [point.xy_projection for point in points]
+        return points
 
     @property
     def all_coordinates(self) -> Iterable[Vector3D]:
-        return (self.start + self.__unit_vector * i for i in range(self.shape_length))
+        return (self.start + self.unit_vector * i for i in range(self.shape_length))
+
+    @property
+    @abstractmethod
+    def unit_vector(self) -> Vector3D:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def shape_dimension(self) -> int:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def height_of_shape(self) -> int:
+        raise NotImplementedError
+
+    @abstractmethod
+    def point_could_fall_on_brick(self, point: Vector3D) -> bool:
+        raise NotImplementedError
+
+
+class XBrick(Brick):
+    __unit_vector = Vector3D((1, 0, 0))
+
+    @property
+    def shape_dimension(self) -> int:
+        return 0
+
+    @property
+    def unit_vector(self) -> Vector3D:
+        return self.__unit_vector
+
+    @property
+    def height_of_shape(self) -> int:
+        return 1
+
+    def point_could_fall_on_brick(self, point: Vector3D) -> bool:
+        if not self.is_below(point):
+            return False
+        return point.y == self.xy_projection.y and self.start.x <= point.x <= self.end.x
+
+
+class YBrick(Brick):
+    __unit_vector = Vector3D((0, 1, 0))
+
+    @property
+    def shape_dimension(self) -> int:
+        return 1
+
+    @property
+    def unit_vector(self) -> Vector3D:
+        return self.__unit_vector
+
+    @property
+    def height_of_shape(self) -> int:
+        return 1
+
+    def point_could_fall_on_brick(self, point: Vector3D) -> bool:
+        if not self.is_below(point):
+            return False
+        return point.x == self.xy_projection.x and self.start.y <= point.y <= self.end.y
+
+
+class ZBrick(Brick):
+    __unit_vector = Vector3D((0, 0, 1))
+
+    @property
+    def shape_dimension(self) -> int:
+        return 2
+
+    @property
+    def unit_vector(self) -> Vector3D:
+        return self.__unit_vector
+
+    @property
+    def height_of_shape(self) -> int:
+        return self.end.z - self.start.z + 1
+
+    def point_could_fall_on_brick(self, point: Vector3D) -> bool:
+        if not self.is_below(point):
+            return False
+        return point.xy_projection == self.xy_projection
+
+    def frontier(self) -> list[Vector3D]:
+        return [self.start.xy_projection]
+
+
+class BrickFactory:
+    _brick_dim_to_type = {0: XBrick, 1: YBrick, 2: ZBrick}
 
     @classmethod
-    def from_line(cls, line: str):
+    def from_line(cls, line: str) -> Brick:
         phrase_halves = line.split('~')
         start = Vector3D(tuple(int(x) for x in phrase_halves[0].split(',')))
         end = Vector3D(tuple(int(x) for x in phrase_halves[1].split(',')))
-        return cls(start, end)
+        shape = end - start
+        shape_dimension = shape.first_non_zero_dim_index
+        return cls._brick_dim_to_type[shape_dimension](start, end, shape)
+
+
+class BrickFrontier:
+    def __init__(self, x_size: int, y_size: int):
+        self.x_max = x_size + 1
+        self.y_max = y_size + 1
+        self.data = [[0 for _ in range(self.x_max)] for _ in range(self.y_max)]
+
+    def __repr__(self) -> str:
+        return f"Frontier: {self.data}"
+
+    def __getitem__(self, item: tuple[int, int]) -> int:
+        return self.data[item[0]][item[1]]
+
+    def __setitem__(self, key: tuple[int, int], value: int) -> None:
+        self.data[key[0]][key[1]] = value
+        return None
+
+    def add(self, points: list[Vector3D], height_of_shape: int) -> int:
+        max_height_below = max(self[(point.x, point.y)] for point in points)
+        new_height = max_height_below + height_of_shape
+        for point in points:
+            self[(point.x, point.y)] = new_height
+        return max_height_below
 
 
 class FallingBricks:
     """What's that in the sky? OMG RUN"""
+
     def __init__(self, bricks: list[Brick]):
         self.bricks = self.__sorted_bricks(bricks)
+
+    def __len__(self) -> int:
+        return len(self.bricks)
 
     def __getitem__(self, item: int) -> Brick:
         return self.bricks[item]
@@ -132,31 +208,43 @@ class FallingBricks:
     def __repr__(self) -> str:
         return f"{self.bricks}"
 
-    def bricks_supporting(self, brick_index: int) -> list[int]:
-        if brick_index == 0:
-            return []
+    @property
+    def max_x(self) -> int:
+        maximum = 0
+        for brick in self.bricks:
+            maximum = max(maximum, brick.xy_projection.x)
+        return maximum
 
-        comparison_brick = self[brick_index]
+    @property
+    def max_y(self) -> int:
+        maximum = 0
+        for brick in self.bricks:
+            maximum = max(maximum, brick.xy_projection.y)
+        return maximum
 
-        max_coordinates_left_to_check = comparison_brick.shape_length
-        occlusions = [0 for _ in range(max_coordinates_left_to_check)]
-        supporting_indices = []
+    def brick_supports(self) -> dict[int, list[int]]:
+        n = len(self)
+        brick_to_brick_supports = [[] for _ in range(n)]
+        height_to_bricks = defaultdict(list)
+        max_x = self.max_x
+        max_y = self.max_y
+        frontier = BrickFrontier(max_x, max_y)
+        frontier.add(self[0].frontier(), self[0].height_of_shape)
+        height_to_bricks[0].append(0)
+        print(frontier, "adding brick frontier:", self[0].frontier(), brick_to_brick_supports, height_to_bricks)
+        for i in range(1, n):
+            brick = self[i]
+            brick_frontier = brick.frontier()
+            max_height_below = frontier.add(brick_frontier, brick.height_of_shape)
+            height_to_bricks[max_height_below].append(i)
+            print(frontier, "adding brick frontier:", brick_frontier, brick_to_brick_supports, height_to_bricks)
 
-        for i in range(brick_index-1, -1, -1):
-            print(f"Checking brick: {brick_index} supported by {i}")
-            if max_coordinates_left_to_check == 0:
-                return supporting_indices
-            intersections = comparison_brick.insersections(self[i])
-            unoccluded_intersections = [x for x in intersections if not occlusions[x]]
-            if any(unoccluded_intersections):
-                for intersection in unoccluded_intersections:
-                    occlusions[intersection] = 1
-                supporting_indices.append(i)
-                max_coordinates_left_to_check -= 1
-        print(f"Supporting brick {brick_index} are: {supporting_indices}")
-        print('-' * 50)
-        supporting_indices.reverse()
-        return supporting_indices
+        values = list(height_to_bricks.values())
+        for i in range(len(height_to_bricks)-1, 0, -1):
+            for brick_index in values[i]:
+                brick_to_brick_supports[brick_index] = values[i-1]
+
+        return brick_to_brick_supports
 
     @staticmethod
     def __sort_key(b: Brick) -> int:
@@ -167,15 +255,17 @@ class FallingBricks:
 
 
 def tests():
-    falling_bricks = FallingBricks([Brick.from_line(line) for line in read_lines("day_22_1_test_input.txt")])
+    falling_bricks = FallingBricks([BrickFactory.from_line(line) for line in read_lines("day_22_1_test_input.txt")])
     print(falling_bricks)
-    assert falling_bricks.bricks_supporting(0) == []  # A
-    assert falling_bricks.bricks_supporting(1) == [0]  # B
-    assert falling_bricks.bricks_supporting(2) == [0]  # C
-    assert falling_bricks.bricks_supporting(3) == [1, 2]  # D
-    assert falling_bricks.bricks_supporting(4) == [1, 2]  # E
-    assert falling_bricks.bricks_supporting(5) == [3, 4]  # F
-    assert falling_bricks.bricks_supporting(6) == [5]  # G
+    brick_supports = falling_bricks.brick_supports()
+    print(brick_supports)
+    assert brick_supports[0] == []  # A
+    assert brick_supports[1] == [0]  # B
+    assert brick_supports[2] == [0]  # C
+    assert brick_supports[3] == [1, 2]  # D
+    assert brick_supports[4] == [1, 2]  # E
+    assert brick_supports[5] == [3, 4]  # F
+    assert brick_supports[6] == [5]  # G
 
 
 def main():
