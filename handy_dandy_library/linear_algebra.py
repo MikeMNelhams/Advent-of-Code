72f909414bd927ae5cc66d45966e3d02dc18380c
MikeMNelhams <mikemnelhams@gmail.com>
2024-01-16 14:14:06 +0000

    Changelist:
    - Refactored to use the linear_algebra.py class in day 24_1

--- handy_dandy_library/linear_algebra.py
+++ handy_dandy_library/linear_algebra.py
@@ -59,6 +59,65 @@ class Vector(UnitVector):
         return cls((0, 0))
 
 
+class Vector3D:
+    def __init__(self, values: tuple[int, int, int]):
+        self.x = values[0]
+        self.y = values[1]
+        self.z = values[2]
+
+    def __eq__(self, other: Vector3D) -> bool:
+        return self.x == other.x and self.y == other.y and self.z == other.z
+
+    def __repr__(self) -> str:
+        return f"({self.x}, {self.y}, {self.z})"
+
+    def __add__(self, other: Vector3D) -> Vector3D:
+        return Vector3D((self.x + other.x, self.y + other.y, self.z + other.z))
+
+    def __sub__(self, other: Vector3D) -> Vector3D:
+        return Vector3D((self.x - other.x, self.y - other.y, self.z - other.z))
+
+    def __mul__(self, other: int) -> Vector3D:
+        return Vector3D((self.x * other, self.y * other, self.z * other))
+
+    def copy(self):
+        return Vector3D((self.x, self.y, self.z))
+
+    @property
+    def magnitude(self) -> int:
+        return (self.x**2 + self.y**2 + self.z**2) ** 0.5
+
+    @property
+    def unit_vector(self) -> Vector3D:
+        magnitude = self.xy_projection.magnitude
+        if magnitude == 0:
+            raise ZeroDivisionError
+        return self * (1 / magnitude)
+
+    @property
+    def xy_projection(self) -> Vector3D:
+        return Vector3D((self.x, self.y, 0))
+
+    @property
+    def manhattan_distance_from_zero(self) -> int:
+        return self.x + self.y + self.z
+
+    @property
+    def first_non_zero_dim_index(self) -> int:
+        for i, value in enumerate(self.values):
+            if value != 0:
+                return i
+        raise TypeError
+
+    @property
+    def values(self) -> list[int]:
+        return [self.x, self.y, self.z]
+
+    @classmethod
+    def zero(cls):
+        return cls((0, 0, 0))
+
+
 def polygon_area(polygon: list[Vector]) -> float:
     return 0.5 * abs(sum(a.cross_product(b) for a, b in consecutive_pairs(polygon)))
 
