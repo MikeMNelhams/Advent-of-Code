40ba1bf05636187e19b4393e82c9f573f010042f
MikeMNelhams <mikemnelhams@gmail.com>
2024-01-25 01:27:42 +0000

    Changelist:
    - Day 5 was a pain point of mine for refactoring... But, it's refactored

--- solutions/day5/day5_1.py
+++ solutions/day5/day5_1.py
@@ -1,12 +1,22 @@
+from __future__ import annotations
 from handy_dandy_library.file_processing import read_lines
+
+import sys
+
+from functools import reduce
+from itertools import chain
 
 
 class Map:
     def __init__(self, map_ranges: tuple[int]):
         self.interval_mappings = self.__get_interval_mappings(map_ranges)
+        self.vals = map_ranges
 
     def __getitem__(self, item: int):
         return self.interval_mappings[item]
+
+    def __repr__(self) -> str:
+        return f"Map{self.interval_mappings}"
 
     @staticmethod
     def __get_interval_mappings(map_ranges: list[int]) -> tuple[int]:
@@ -15,7 +25,7 @@ class Map:
         addition_coefficient = map_ranges[0] - start
         return start, end, addition_coefficient
 
-    def overlaps(self, other) -> bool:
+    def overlaps(self, other: Map) -> bool:
         first_overlap_check = self.interval_mappings[1] > other.interval_mappings[0]
         return first_overlap_check or other.interval_mappings[1] > self.interval_mappings[0]
 
@@ -25,8 +35,9 @@ class Map:
     def destination_assuming_valid(self, source: int) -> int:
         return source + self.interval_mappings[2]
 
-    def __repr__(self) -> str:
-        return f"Map{self.interval_mappings}"
+    @property
+    def inverted(self) -> Map:
+        return Map((self.vals[1], self.vals[0], self.vals[2]))
 
 
 class FarmingDataReader:
@@ -44,7 +55,7 @@ class FarmingDataReader:
     def start_index(self) -> int:
         return self.__start_index
 
-    def parse_seed_numbers(self) -> list[int]:
+    def seed(self) -> list[int]:
         seeds_phrase = None
 
         for i, line in enumerate(self.__lines):
@@ -75,41 +86,95 @@ class FarmingDataReader:
         return map_numbers
 
 
-def lowest_location(seeds: list[int], maps: list[Map]) -> int:
-    seeds_copy = seeds.copy()
-    for mappings in maps:
-        print(mappings)
-        seeds_copy = destination(seeds_copy, mappings)
-    return min(seeds_copy)
+class Farmer:
+    def __init__(self, start_seeds: list[int], start_maps: list[list[Map]]):
+        self.start_seeds = start_seeds
+        self.start_maps = start_maps
+
+    @classmethod
+    def from_farming_data_reader(cls, farming_data_reader: FarmingDataReader):
+        seeds = farming_data_reader.seed()
+        maps = [farming_data_reader.parse_next_source_to_destination_map() for _ in range(7)]
+        return cls(seeds, maps)
+
+    @staticmethod
+    def __destination(seeds: list[int], mappings: Map) -> list[int]:
+        next_seeds = seeds.copy()
+        for i, seed in enumerate(seeds):
+            for mapping in mappings:
+                if mapping.is_within_mapping(seed):
+                    next_seeds[i] = mapping.destination_assuming_valid(seed)
+                    break
+        return next_seeds
+
+    @property
+    def lowest_location(self) -> int:
+        final_seeds = reduce(self.__destination, self.start_maps, self.start_seeds)
+        return min(final_seeds)
+
+    @property
+    def transformed_seeds(self) -> list[int]:
+        new_seeds = self.start_seeds.copy()
+        for i in range(0, len(self.start_seeds), 2):
+            new_seeds[i + 1] = new_seeds[i] + new_seeds[i + 1] - 1
+        new_seeds.sort()
+        return new_seeds
+
+    @property
+    def lowest_possible_location(self) -> int:
+        seed_ends = (s for s in self.__seed_endpoints if self.in_transformed_seed_range(s))
+        return min(self.seed_to_location(s) for s in seed_ends)
+
+    @property
+    def __seed_endpoints(self) -> list[int]:
+        return reduce(invert_maps, reversed(self.start_maps), (0, sys.maxsize))
+
+    def seed_to_location(self, seed: int) -> int:
+        return reduce(destination_from_maps, self.start_maps, seed)
+
+    def in_transformed_seed_range(self, test_seed: int) -> bool:
+        s = self.transformed_seeds
+        return any(s[i] <= test_seed <= s[i + 1] for i in range(0, len(s), 2))
 
 
-def destination(seeds: list[int], maps: list[Map]) -> list[int]:
-    next_seeds = seeds.copy()
-    for i, seed in enumerate(seeds):
-        for mapping in maps:
-            if mapping.is_within_mapping(seed):
-                next_seeds[i] = mapping.destination_assuming_valid(seed)
-                break
-    return next_seeds
+def invert_maps(output_ends: list[int], maps: list[Map]) -> list[int]:
+    map_ends = (((_map.vals[0], _map.vals[1]), (_map.vals[0] + _map.vals[2] - 1, _map.vals[0] + _map.vals[2] - 1))
+                for _map in maps)
+
+    input_src_ends = list(set(x for y, x in chain.from_iterable(map_ends)))
+    if input_src_ends[0] > 0:
+        input_src_ends = [0, input_src_ends[0] - 1] + input_src_ends
+    if input_src_ends[-1] < sys.maxsize:
+        input_src_ends = input_src_ends + [input_src_ends[-1] + 1, sys.maxsize]
+
+    inverted_maps = [_map.inverted for _map in maps]
+    output_src_ends = (destination_from_maps(y, inverted_maps) for y in output_ends)
+
+    return list(set(output_src_ends) | set(input_src_ends))
 
 
-def lowest_location_from_reader(farming_data_reader: FarmingDataReader) -> int:
-    seeds = farming_data_reader.parse_seed_numbers()
-    maps = [farming_data_reader.parse_next_source_to_destination_map() for _ in range(7)]
-    return lowest_location(seeds, maps)
+def destination_from_maps(x: int, maps: list[Map]) -> int:
+    for _map in maps:
+        if _map.is_within_mapping(x):
+            return _map.destination_assuming_valid(x)
+    return x
 
 
 def tests():
     farming_data_reader = FarmingDataReader("day_5_1_test_input.txt")
-    assert lowest_location_from_reader(farming_data_reader) == 35
+    farmer = Farmer.from_farming_data_reader(farming_data_reader)
+    t = farmer.lowest_location
+    assert t == 35
 
 
 def main():
     tests()
 
-    # farming_data_reader = FarmingDataReader("day_5_1_input.txt")
-    # t = lowest_location_from_reader(farming_data_reader)
-    # print(t)
+    farming_data_reader = FarmingDataReader("day_5_1_input.txt")
+    farmer = Farmer.from_farming_data_reader(farming_data_reader)
+    t = farmer.lowest_location
+    print(t)
+    assert t == 107430936
 
 
 if __name__ == "__main__":
--- solutions/day5/day_5_2.py
+++ solutions/day5/day_5_2.py
@@ -1,68 +1,22 @@
-import sys
-from bisect import insort, bisect_left, bisect_right
-from handy_dandy_library.file_processing import read_lines
-from day5_1 import FarmingDataReader, Map
-
-
-PARTITION_DEFAULT = [0, sys.maxsize]
-
-
-class IntervalPartitionsList:
-    def __init__(self):
-        self.interval_partitions = []
-
-    def __len__(self):
-        return len(self.interval_partitions)
-
-    def add_mappings(self, mappings: list[Map]) -> None:
-        self.interval_partitions.append(PARTITION_DEFAULT.copy())
-        lowest = sys.maxsize
-        highest = -1
-        for mapping in mappings:
-            print(f"adding mapping: {mapping}")
-            low = mapping[0]
-            high = mapping[1]
-            highest = max(highest, high)
-            if low == 0:
-                lowest = min(lowest, high)
-            if low != 0:
-                lowest = min(lowest, low)
-                insort(self.interval_partitions[-1], low)
-            if high != low:
-                insort(self.interval_partitions[-1], high)
-
-        if lowest != 0:
-            insort(self.interval_partitions[-1], lowest - 1)
-
-        insort(self.interval_partitions[-1], highest + 1)
-        print(self.interval_partitions[-1])
-        return None
-
-    def __repr__(self) -> str:
-        return f"Mapping intervals: {self.interval_partitions}"
-
-
-def location_low2_from_data_reader(farming_data_reader: FarmingDataReader) -> int:
-    intervals = IntervalPartitionsList()
-    seeds = farming_data_reader.parse_seed_numbers()
-    maps = [farming_data_reader.parse_next_source_to_destination_map() for _ in range(7)]
-    for mappings in reversed(maps):
-        print(mappings)
-        intervals.add_mappings(mappings)
-        print('_' * 200)
-
-    print('-'*200)
-    print("FINAL")
-    print(intervals)
+from day5_1 import FarmingDataReader, Farmer
 
 
 def tests():
     farming_data_reader = FarmingDataReader("day_5_1_test_input.txt")
-    location_low2_from_data_reader(farming_data_reader)
+    farmer = Farmer.from_farming_data_reader(farming_data_reader)
+
+    t = farmer.lowest_possible_location
+    assert t == 46
 
 
 def main():
     tests()
+
+    farming_data_reader = FarmingDataReader("day_5_1_input.txt")
+    farmer = Farmer.from_farming_data_reader(farming_data_reader)
+
+    t = farmer.lowest_possible_location
+    assert t == 23738616
 
 
 if __name__ == "__main__":
