0efc36a8bb107bfde073ce87973a31a829b96c63
MikeMNelhams <mikemnelhams@gmail.com>
2025-01-25 16:10:52 +0000

    Changelist:
    - I'm really losing my momentum working on these dang pathfinding problems over and over. Hopefully next year is a tad more interesting.
    - Anyway, day 18 part 1 done. A* algorithm.

---
+++ solutions/2024/day18/day18_1.py
@@ -0,0 +1,165 @@
+from handy_dandy_library.file_processing import read_lines
+from handy_dandy_library.linear_algebra import Vector2D
+
+from typing import Self
+from heapq import heappop, heappush
+from collections import defaultdict
+
+CameFromDict = dict[Vector2D, Vector2D]
+
+
+class FallingByteGrid:
+    UP = Vector2D((0, -1))
+    RIGHT = Vector2D((1, 0))
+    DOWN = Vector2D((0, 1))
+    LEFT = Vector2D((-1, 0))
+
+    INF = 10 ** 10
+
+    directions = (UP, RIGHT, DOWN, LEFT)
+
+    def __init__(self, lines: list[str], width: int, height: int):
+        self.n = len(lines)
+        self.width = width
+        self.height = height
+        self.fallen_bytes = self.__fallen_bytes(lines)
+        self.fallen_bytes_set = set()
+
+        self.start = Vector2D((0, 0))
+        self.end = Vector2D((self.width - 1, self.height - 1))
+
+        self.g_scores = defaultdict(lambda: self.INF)
+        self.h_scores = {Vector2D((i, j)): self.end.manhattan_distance(Vector2D((i, j))) for j in range(self.width) for i in range(self.height)}
+
+    def print_path(self, path: list[Vector2D]) -> None:
+        grid = [['.' for _ in range(self.height)] for _ in range(self.width)]
+
+        for fallen_byte in self.fallen_bytes:
+            grid[fallen_byte.y][fallen_byte.x] = '#'
+
+        for coordinate in path:
+            grid[coordinate.y][coordinate.x] = 'o'
+
+        print('\n'.join(''.join(row) for row in grid))
+        return None
+
+    @staticmethod
+    def __fallen_bytes(lines: list[str]) -> list[Vector2D]:
+        falling_bytes = [[int(x) for x in line.split(',')] for line in lines]
+        return [Vector2D((x[0], x[1])) for x in falling_bytes]
+
+    def minimum_steps(self, number_of_bytes: int) -> int:
+        self.fallen_bytes = self.fallen_bytes[:number_of_bytes]
+        self.fallen_bytes_set = set()
+
+        for coordinate in self.fallen_bytes:
+            self.fallen_bytes_set.add(coordinate)
+            self.g_scores[coordinate] = -1
+            self.h_scores[coordinate] = -1
+
+        best_previous: CameFromDict = {self.start: (Vector2D((-1, -1)))}
+
+        self.__set_g_score(self.start, best_previous)
+
+        to_check = [State(self.h(self.start), self.start)]
+        to_check_set = {self.start}
+
+        while to_check:
+            state = heappop(to_check)
+
+            node = state.coordinate
+            f_score = state.f
+
+            to_check_set.remove(node)
+
+            if node == self.end:
+                path_back = self.reconstruct_path(best_previous, self.end)
+                self.print_path(path_back)
+
+                return f_score + 1
+
+            neighbours = [neighbour for direction in self.directions
+                          if (neighbour := node + direction) not in self.fallen_bytes_set and self.is_within_grid(neighbour)]
+
+            for neighbour in neighbours:
+                tentative_g_score = self.__get_g_score(node)
+                if tentative_g_score < self.__get_g_score(neighbour):
+                    best_previous[neighbour] = node
+                    self.__set_g_score(neighbour, best_previous)
+                    neighbour_h_score = self.h(neighbour)
+                    f_score = tentative_g_score + neighbour_h_score
+
+                    if neighbour not in to_check_set:
+                        heappush(to_check, State(f_score, neighbour))
+                        to_check_set.add(neighbour)
+
+        return -1
+
+    @staticmethod
+    def reconstruct_path(best_previous: CameFromDict, coordinate: Vector2D) -> list[Vector2D]:
+        total_path = [coordinate]
+        current = coordinate
+        while current in best_previous:
+            current = best_previous[current]
+            total_path.append(current)
+        total_path.reverse()
+        return total_path
+
+    def is_within_grid(self, coordinate: Vector2D) -> bool:
+        return (0 <= coordinate.x < self.width) and (0 <= coordinate.y < self.height)
+
+    def is_fallen_byte(self, coordinate: Vector2D) -> bool:
+        return
+
+    def __get_g_score(self, coordinate: Vector2D) -> int:
+        return self.g_scores[coordinate]
+
+    def __set_g_score(self, coordinate: Vector2D, best_previous: CameFromDict) -> None:
+        assert coordinate not in self.fallen_bytes_set, ValueError("cannot change the score of a wall!")
+        if coordinate == self.start:
+            self.g_scores[coordinate] = 0
+            return None
+
+        previous_coordinate = best_previous[coordinate]
+        previous_g_score = self.__get_g_score(previous_coordinate)
+
+        self.g_scores[coordinate] = previous_g_score + 1
+        return None
+
+    def h(self, coordinate: Vector2D) -> int:
+        return self.h_scores[coordinate]
+
+
+class State:
+    def __init__(self, f_score: int, coordinate: Vector2D):
+        self.f = f_score
+        self.coordinate = coordinate
+
+    def __repr__(self) -> str:
+        return f"State[{self.f}, {self.coordinate}]"
+
+    def __lt__(self, other: Self) -> bool:
+        return self.f < other.f
+
+    def __eq__(self, other: Self) -> bool:
+        return self.f == other.f
+
+
+def tests():
+    fallen_byte_grid = FallingByteGrid(read_lines("puzzle18_1_test_input1.txt"), 7, 7)
+
+    t1 = fallen_byte_grid.minimum_steps(12)
+    assert t1 == 22
+
+
+def main():
+    tests()
+
+    fallen_byte_grid = FallingByteGrid(read_lines("puzzle18_1.txt"), 71, 71)
+
+    t1 = fallen_byte_grid.minimum_steps(1024)
+    assert t1 == 292
+
+
+if __name__ == "__main__":
+    main()
